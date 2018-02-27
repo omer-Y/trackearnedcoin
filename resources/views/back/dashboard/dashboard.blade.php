fd27324b19e67b65d9d833789d774f89fff07c2d
Ömer Yelkenci <om.yelkenci@outlook.com>
2018-02-27 17:36:56 +0300

    Dashboard itemleri eklendi.

--- resources/views/back/dashboard/dashboard.blade.php
+++ resources/views/back/dashboard/dashboard.blade.php
@@ -1,6 +1,222 @@
 @extends('back.cover')
 @section('content')
+    <div id='mainContent'>
+        <div class="row gap-20 masonry pos-r">
+            <div class="masonry-sizer col-md-6"></div>
+            <div class="masonry-item  w-100">
+                <div class="row gap-20">
+                    <!-- #Toatl Visits ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Total Visits</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-green-50 c-green-500">+10%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
+                    <!-- #Total Page Views ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Total Page Views</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash2"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-red-50 c-red-500">-7%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- #Unique Visitors ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Unique Visitor</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash3"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-purple-50 c-purple-500">~12%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- #Bounce Rate ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Bounce Rate</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash4"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-blue-50 c-blue-500">33%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="masonry-item  w-100">
+                <div class="row gap-20">
+                    <!-- #Toatl Visits ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Total Visits</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-green-50 c-green-500">+10%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- #Total Page Views ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Total Page Views</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash2"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-red-50 c-red-500">-7%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- #Unique Visitors ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Unique Visitor</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash3"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-purple-50 c-purple-500">~12%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- #Bounce Rate ==================== -->
+                    <div class='col-md-3'>
+                        <div class="layers bd bgc-white p-20">
+                            <div class="layer w-100 mB-10">
+                                <h6 class="lh-1">Bounce Rate</h6>
+                            </div>
+                            <div class="layer w-100">
+                                <div class="peers ai-sb fxw-nw">
+                                    <div class="peer peer-greed">
+                                        <span id="sparklinedash4"></span>
+                                    </div>
+                                    <div class="peer">
+                                        <span class="d-ib lh-0 va-m fw-600 bdrs-10em pX-15 pY-15 bgc-blue-50 c-blue-500">33%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="masonry-item  w-100">
+                <div class="row gap-20">
+                    <div class="col-md-12">
+                        <!-- #Monthly Stats ==================== -->
+                        <div class="bd bgc-white">
+                            <div class="layers">
+                                <div class="layer w-100 pX-20 pT-20">
+                                    <h6 class="lh-">Monthly Stats</h6>
+                                </div>
+                                <div class="layer w-100 p-20">
+                                    <canvas id="line-chart" height="220"></canvas>
+                                </div>
+                                <div class="layer bdT p-20 w-100">
+                                    <div class="peers ai-c jc-c gapX-20">
+                                        <div class="peer">
+                                            <span class="fsz-def fw-600 mR-10 c-grey-800">10% <i class="fa fa-level-up c-green-500"></i></span>
+                                            <small class="c-grey-500 fw-600">APPL</small>
+                                        </div>
+                                        <div class="peer fw-600">
+                                            <span class="fsz-def fw-600 mR-10 c-grey-800">2% <i class="fa fa-level-down c-red-500"></i></span>
+                                            <small class="c-grey-500 fw-600">Average</small>
+                                        </div>
+                                        <div class="peer fw-600">
+                                            <span class="fsz-def fw-600 mR-10 c-grey-800">15% <i class="fa fa-level-up c-green-500"></i></span>
+                                            <small class="c-grey-500 fw-600">Sales</small>
+                                        </div>
+                                        <div class="peer fw-600">
+                                            <span class="fsz-def fw-600 mR-10 c-grey-800">8% <i class="fa fa-level-down c-red-500"></i></span>
+                                            <small class="c-grey-500 fw-600">Profit</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <!-- #Site Visits ==================== -->
+                        <div class="bd bgc-white">
+                            <div class="peers fxw-nw@lg+ ai-s">
+                                <div class="peer peer-greed w-70p@lg+ w-100@lg- p-20">
+                                    <div class="layers">
+                                        <div class="layer w-100 mB-10">
+                                            <h6 class="lh-1">Site Visits</h6>
+                                        </div>
+                                        <div class="layer w-100">
+                                            <div id="world-map-marker"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('title')
     @lang('dashboard/title.title')
